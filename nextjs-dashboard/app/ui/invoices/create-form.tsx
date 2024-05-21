1ceb23b350d9056b0794de868f3d439866b85e8d
jamesbrown173 <jamesbrown173@gmail.com>
2024-05-21 13:47:43 +0700

    Update create-form.tsx

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -12,6 +12,8 @@ import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
 
+// commit 1
+
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
